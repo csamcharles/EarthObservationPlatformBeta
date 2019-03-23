501e7279334ddf33a79ca0fd9868fe5d019a6f58
csamcharles <c.sam.charles@gmail.com>
2019-03-22 19:21:57 -0700

    add api post request to work with python cli, server runs but need to debug file download

--- api/app.js
+++ api/app.js
@@ -1,2 +1,27 @@
-import express from 'express'
+const bodyParser = require('body-parser');
+const express = require('express');
+const fileUpload = require('express-fileupload');
 
+const app = express();
+
+app.use(fileUpload());
+
+const PORT = 8080;
+
+app.post('/api/submit-alg', (req, res) => {
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let newAlg = req.files.newAlg
+    newAlg.mv(`./db/${newAlg.name}`, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.status(200).send('File Sent!')
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`)
+})
--- cli/main.py
+++ cli/main.py
@@ -1,10 +1,10 @@
 import os.path
 import requests
 
-local = False
+local = True
 
 # put the hosted endpoint here
-api_post = 'http://localhost:8080/post' if local else 'aws.com'
+api_post = 'http://localhost:8080/api/submit-alg' if local else 'aws.com'
 
 print("Please enter the path to your algorithm: ")
 
@@ -14,6 +14,7 @@ if os.path.isfile(file):
     print("Sending your algorithm to the cloud...")
     with open(file, 'rb') as f:
         response = requests.post(api_post, files={file: f})
+        print(response)
 else:
     print("Error! this is not a valid file.")
     exit()
