053a1361d2d061fb74c7e4ca47d054d2d8de1282
Conor Butte-Landsfried <cblands@uvic.ca>
2019-04-10 00:02:29 -0700

    Added cloud connectivity

--- cli/main.py
+++ cli/main.py
@@ -1,10 +1,10 @@
 import os.path
 import requests
 
-local = True
+local = False
 
 # put the hosted endpoint here
-api_post = 'http://localhost:8080/api/submit-alg' if local else 'aws.com'
+api_post = 'http://localhost:8080/api/submit-alg' if local else 'http://54.69.235.20:8080/api/submit-alg'
 
 print("Please enter the path to your algorithm: ")
 
